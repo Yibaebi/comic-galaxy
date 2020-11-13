7e474c3813f83b287a64b23e9b572f01bc7b82a3
Eagle <eobumma@gmail.com>
2020-11-13 11:01:46 +0100

    add footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import NavBar from "./NavBar";
+import Footer from "./Footer";
 import search from "./icons/search-icon.png";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
         <img src={search} alt="search bar"/>
         <input className="search-input" type="search" placeholder="Search Comics" />
       </form>
+      <Footer />
     </div>
   );
 }
--- src/Footer.js
+++ src/Footer.js
@@ -1,17 +1,51 @@
 import React from 'react';
+import logo from './images/logo.png';
+import './Footer.css';
+import {
+    FaYoutube,
+    FaTwitter,
+    FaLinkedinIn,
+} from 'react-icons/fa';
+
 
 class Footer extends React.Component {
     render() {
         return (
             <footer>
                 <div className="footer-container">
-                    <div className="footer-item">
-                        <img src="./logo.png" alt="logo.png" />
+                    <div className="footer-item-a">
+                        <img src={logo} alt="logo.png" />
                     </div>
-                    <div className="footer-item">
+                    <div className="footer-item-b">
+                        <a
+                            className='social-icon-link'
+                            to='/'
+                            target='_blank'
+                            aria-label='Youtube'
+                        >
+                            <FaYoutube />
+                        </a>
+                        <a
+                            className='social-icon-link'
+                            to='/'
+                            target='_blank'
+                            aria-label='Youtube'
+                        >
+                            <FaTwitter />
+                        </a>
+                        <a
+                            className='social-icon-link'
+                            to='/'
+                            target='_blank'
+                            aria-label='Youtube'
+                        >
+                            <FaLinkedinIn />
+                        </a>
                     </div>
                 </div>
             </footer>
         )
     }
 }
+
+export default Footer;
