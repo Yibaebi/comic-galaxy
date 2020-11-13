52ce278a00cb766500d9d6f2e2fac34542554c90
Yibaebi Elliot <59321440+Yibaebi@users.noreply.github.com>
2020-11-13 07:06:15 +0100

    Initial Commit

---
+++ src/NavBar.js
@@ -0,0 +1,60 @@
+import React from "react";
+
+class NavBar extends React.Component {
+  state = {
+    showDropdown: "hide",
+  };
+
+  handleMouseOver = () => {
+    this.setState({
+      showDropdown: "show",
+    });
+  };
+
+  handleMouseLeave = () => {
+    this.setState({
+      showDropdown: "hide",
+    });
+  };
+
+  render() {
+    return (
+      <header className="main-header">
+        <div>
+          <img src={""} alt="comic galaxy logo"></img>
+        </div>
+        <div className="header-links">
+          <ul className="link-container">
+            <li>
+              <a href="/Home"> Home </a>
+            </li>
+            <li>
+              <a href="/Home"> Support </a>
+            </li>
+            <li>
+              <a href="/Home"> Gallery </a>
+            </li>
+            <li
+              id="artists"
+              onMouseLeave={this.handleMouseLeave}
+            >
+              <a href="/Home" id="for-artists" onMouseOver={this.handleMouseOver}>
+                For Artists
+              </a>
+              <div className="dropdown-list" id={this.state.showDropdown}>
+                <a href="/Signup" id="sign-up">
+                  Sign up
+                </a>
+                <a href="/Login" id="login">
+                  Login
+                </a>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </header>
+    );
+  }
+}
+
+export default NavBar;
